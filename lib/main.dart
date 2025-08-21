d900f22d785ad3a4b00b1f5860111b32e65667c5
likeshkumaryadav <likesh@smartsys.in>
2025-08-21 17:34:38 +0530

    firabse web issue has been fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:smart_inc_authapp/firebase_options.dart';
 import 'features/auth/presentation/auth_gate.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: CountlyApp()));
 }
 
